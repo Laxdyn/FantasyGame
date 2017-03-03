79732bc19ed82e248468d4d50593fc4240060b41
Brad Gustafson <greeninja13@gmail.com>
2017-03-03 12:02:35 -0700

    MVC
    Model, view, and controller separated.

---
+++ FantasyGame/src/Controller/FantasyGame.java
@@ -0,0 +1,11 @@
+package Controller;
+
+import View.GameWindow;
+
+public class FantasyGame {
+
+	public static void main(String[] args) throws InterruptedException{
+		GameWindow gw = new GameWindow();
+		gw.initialize();
+	}
+}
